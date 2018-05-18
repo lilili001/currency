0c9d572b3e56d865d5ea4107dee70c2b01c425ca
lilili001 <2861166132@qq.com>
2018-05-18 15:34:01 +0800

    add sql

--- Composers/CurrencyAllowedViewComposer.php
+++ Composers/CurrencyAllowedViewComposer.php
@@ -15,20 +15,25 @@ class CurrencyAllowedViewComposer
     /**
      * CurrencyAllowedViewComposer constructor.
      */
-    public function __construct(CurrencyRepository $currency, CurrencySymbolRepository $currencySymbol)
-    {
-        $this->currency = $currency;
-        $this->currencySymbol = $currencySymbol;
-    }
-
+//    public function __construct(CurrencyRepository $currency, CurrencySymbolRepository $currencySymbol)
+//    {
+//        $this->currency = $currency;
+//        $this->currencySymbol = $currencySymbol;
+//    }
+//
+//    public function compose(View $view)
+//    {
+//        $currencyList = $this->currency->getRateList();
+//        $currencySymbo = $this->currencySymbol->all();
+//         array_map(function($item)use( $currencySymbo ){
+//           $currencyTo = $item->currency_to;
+//           $item['symbol'] = $currencySymbo[$currencyTo];
+//         } ,$currencyList );
+//        $view->with('allowdCurrencies',compact('currencyList'));
+//    }
     public function compose(View $view)
     {
-        $currencyList = $this->currency->getRateList();
-        $currencySymbo = $this->currencySymbol->all();
-         array_map(function($item)use( $currencySymbo ){
-           $currencyTo = $item->currency_to;
-           $item['symbol'] = $currencySymbo[$currencyTo];
-         } ,$currencyList );
-        $view->with('allowdCurrencies',compact('currencyList'));
+        $a = 'alice';
+        $view->with('allowdCurrencies' , $a  );
     }
 }
